2c23d78fc3562158d8370fbe504de731fa82de0e
benlepsch <blepsch@gmail.com>
2019-12-13 09:57:16 -0500

    add bean jump first try

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ socketio = SocketIO(app, async_mode='eventlet')
 def index():
     return render_template('index.html')
 
+@app.route('/beanjump')
+def beanjump():
+    return render_template('beanjump.html')
+
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=80)
